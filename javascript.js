1448aaec593871eddde27903257ed9d7e6427f59
kyungminkim1 <kyungminkimnz@gmail.com>
2022-09-26 21:53:38 +1300

    Rename variable to be readable

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@
 function getComputerChoice() {
 
     // choose a random number from 1-3
-    let randomInt = Math.floor(Math.random() * 3) + 1;
+    let randomChoice = Math.floor(Math.random() * 3) + 1;
     
     // find the string that matches this number e.g. 1 = 'rock'
-    switch (randomInt){
+    switch (randomChoice){
         case 1:
             // return the string
             return 'Rock';
