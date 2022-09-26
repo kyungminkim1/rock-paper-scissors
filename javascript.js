a08f56671d854000971f5be564fb9bd047dded7c
kyungminkim1 <kyungminkimnz@gmail.com>
2022-09-26 21:46:30 +1300

    Generate random number from 1-3 inclusive

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
 // create a function getComputerChoice
 function getComputerChoice() {
+
+    // choose a random number from 1-3
+    let randomInt = Math.floor(Math.random() * 3) + 1;
     
+    // find the string that matches this number e.g. 1 = 'rock'
+    // return the string
+
+
 }
 
-// choose a random number from 1-3
-// find the string that matches this number e.g. 1 = 'rock'
-// return the string
+// getComputerChoice();
