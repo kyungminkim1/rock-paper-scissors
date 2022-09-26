676492632b66d2574809af5212f0b9719365c1af
kyungminkim1 <kyungminkimnz@gmail.com>
2022-09-26 21:41:55 +1300

    Create getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 // create a function getComputerChoice
+function getComputerChoice() {
+    
+}
+
 // choose a random number from 1-3
 // find the string that matches this number e.g. 1 = 'rock'
 // return the string
