2846c016460fd22f926c260b67add7fb2ac88cf6
kyungminkim1 <kyungminkimnz@gmail.com>
2022-10-13 17:05:34 +1300

    Refactor score system and display

--- javascript.js
+++ javascript.js
@@ -37,45 +37,59 @@ function playRound(e) {
         || playerSelection === 'Paper' && computerSelection === 'Rock'
         || playerSelection === 'Scissors' && computerSelection === 'Paper'
         ) {
+            // add win to player score and display
+            playerScore++;
+            playerScoreDisplay.textContent = `Player's score: ${playerScore}`;
             // then return player won message
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
+            resultDisplay.textContent = `You Win! ${playerSelection} beats ${computerSelection}\r\n`;
+            checkScore();
+            return;
     }
     // else if player chose rock and computer chose paper
     // or player chose paper and computer chose scissors
     // or player chose scissors and computer chose rock
-    // then return player lost message
     else if (
         playerSelection === 'Rock' && computerSelection === 'Paper'
         || playerSelection === 'Paper' && computerSelection === 'Scissors'
         || playerSelection === 'Scissors' && computerSelection === 'Rock'
         ) {
-            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+            // add win to player score and display
+            computerScore++;
+            computerScoreDisplay.textContent = `Computer's score: ${computerScore}`;
+            // then return player lost message
+            resultDisplay.textContent = `You Lose! ${computerSelection} beats ${playerSelection}\r\n`;
+            checkScore();
+            return;
     }
 
     // else return draw message
     else {
-        return `You Drew! You both picked ${playerSelection}`;
+        resultDisplay.textContent = `You Drew! You both picked ${playerSelection}\n`;
+        return;
     }
 }
 
-// create function game()
-function game() {
-    // track the player's choice and score
-    let playerChoice = null;
-    let playerScore = 0;
-
-    // declare the player's win/loss
-    if (playerScore >= 3) {
-        console.log('You\'re a winner!!!');
-    } else {
-        console.log('You lost! Better luck next time');
+// create function checkScore()
+function checkScore() {
+    // check if someone has reached score of 5
+    if (playerScore >= 5) {
+        resultDisplay.textContent += 'You\'re a winner!!!';
+    } else if (computerScore >= 5) {
+        resultDisplay.textContent += 'You lost! Better luck next time';
     }
 }
 
-game();
+// track the player and computer's score
+let playerScore = 0;
+let computerScore = 0;
 
 // get buttons
 const btns = document.querySelectorAll('button');
+
+// get div displaying scores and result
+const playerScoreDisplay = document.querySelector('div.player-score');
+const computerScoreDisplay = document.querySelector('div.computer-score');
+const resultDisplay = document.querySelector('div.result');
 
 // add event listeners to each button
 // event listener will call playRound
